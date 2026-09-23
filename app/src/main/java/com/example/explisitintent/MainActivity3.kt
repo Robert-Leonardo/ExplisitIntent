c54e09e53dc7501c6b366c01dfe34bd467f3f863
Robert Leonardo <c14240129@john.petra.ac.id>
2026-09-23 19:58:23 +0700

    Topik 2

--- app/src/main/java/com/example/explisitintent/MainActivity3.kt
+++ app/src/main/java/com/example/explisitintent/MainActivity3.kt
@@ -1,32 +1,20 @@
 package com.example.explisitintent
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.content.Intent
 
-class MainActivity : AppCompatActivity() {
+class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main3)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
-        }
-
-        val _btnExplisit1= findViewById<Button>(R.id.btnExplisit1)
-        _btnExplisit1.setOnClickListener {
-            val intent = Intent(
-                this@MainActivity,
-                MainActivity2::class.java
-            )
-            startActivity(intent)
         }
     }
 }
